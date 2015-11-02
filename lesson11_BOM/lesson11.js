4a79713cc3f7fa586f39e0c35980193d16494880
Charlotte Weaver <charlottewsweaver@gmail.com>
2015-11-01 21:22:45 -0500

    Began lesson 11

---
+++ lesson11_BOM/lesson11.js
@@ -0,0 +1,36 @@
+1) Open Chrome
+
+2) Open the console
+Menu -> more tools -> javascript console
+
+3) BOM
+// The Browser Object Model (BOM) let's you interact with the browser
+// using javascript
+
+// window is an object that represents a browsers window, it is also the parent of 
+// all global javascript variables and functions.
+// (This means that almost everything we've learned so far can be prefixed with window)
+// ex. window.prompt();
+
+// You can use the window object to get the size of the browser window
+window.innerHeight // for height
+window.innerWidth // for width
+
+
+// TRY IT
+
+4) screen
+// size (width, height), colorDepth
+
+5) location
+// getting url, setting url , make sure to include http://
+
+6) history
+// back(), forward()
+
+7) cookies
+// What is a cookie
+// Setting a cookie, getting a cookie, deleting a cookie
+// Why would you use a cookie?
+
+8) Project
