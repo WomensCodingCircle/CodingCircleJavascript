955eea910280fce5f169278c2ab18dc6d82b2db4
Charlotte Weaver <charlottewsweaver@gmail.com>
2015-11-09 22:06:32 -0500

    saving at underscore

---
+++ lesson12_libraries/lesson12.js
@@ -0,0 +1,38 @@
+1) Open Chrome
+
+2) Using Javascript Libraries
+// Since Javascript is so popular, there is already code written for almost
+// anything you want to do. 
+
+// We've already used Jquery, let's explore other libraries and how they might be used.
+
+// Here are some examples
+// If you are designing a website and don't want to worry about creating all the User Interface 
+// elements? Try Bootstrap
+// http://getbootstrap.com/
+
+// Need to perform some common operations on arrays or objects?
+// Check out underscore http://underscorejs.org/
+
+// Want to create some amazing interactive charts?
+// Get highcharts http://www.highcharts.com/download
+
+// 3D animations in your future?
+// You have to use three.js http://threejs.org/
+
+// Here's a list of other libraries you might be interested in
+// https://codegeekz.com/collection-of-best-javascript-libraries-for-2014/
+
+
+3) Download underscore
+// Download underscore and save it in lesson12_libraries folder (the one this document 
+// is in)
+// http://underscorejs.org/underscore.js
+
+// Include it using a script tag in example.html
+
+4) Download highcharts
+// Highcharts is a javascript library that allows you to make interactive charts
+// You can get some pretty amazing charts 
+// while very few lines of your own code
+// http://www.highcharts.com/
